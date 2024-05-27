b098b9d711e43b30fab1936a351e69c02abb8f6a
FabioPenedo <fabiopenedo21@gmail.com>
2024-05-27 16:20:50 -0300

    CRUD Scaffolding

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
